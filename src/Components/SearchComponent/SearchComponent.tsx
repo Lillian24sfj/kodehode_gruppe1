2ae7a0607996f96a442506a781ab538b62e89279
Torjus <Torjus24sfj@gmail.com>
2025-02-13 13:22:40 +0100

    style, react-icons

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -1,10 +1,14 @@
 // @ts-expect-error cssmodules
-import style from "./SearchComponent.module.css"
+import style from "./SearchComponent.module.css";
+import { CiSearch } from "react-icons/ci";
 
 export function SearchComponent() {
-    return (
-        <div >
-            <input className={style.search_input_field}type="text"/>
-        </div>
-    )
+  return (
+    <div className={style.search_bar_flex_container}>
+      <input className={style.search_input_field} type="text" />
+      <div className={style.search_magnifying_icon}>
+        <CiSearch />
+      </div>
+    </div>
+  );
 }
