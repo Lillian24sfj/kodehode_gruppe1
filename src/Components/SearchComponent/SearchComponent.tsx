7058603b4b2a7555ddf16838909f366ea6621fcd
Torjus <Torjus24sfj@gmail.com>
2025-02-13 14:23:38 +0100

    border-radius styling fix

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -1,13 +1,13 @@
 // @ts-expect-error cssmodules
 import style from "./SearchComponent.module.css";
-import { CiSearch } from "react-icons/ci";
+import { IoSearch } from "react-icons/io5";
 
 export function SearchComponent() {
   return (
     <div className={style.search_bar_flex_container}>
       <input className={style.search_input_field} type="text" />
       <div className={style.search_magnifying_icon}>
-        <CiSearch />
+        <IoSearch className={style.magnifying_icon}/>
       </div>
     </div>
   );
