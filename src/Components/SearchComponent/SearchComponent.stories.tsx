4d47c53a0c6b8ca658f8be84a82a74d0972f4bbf
Torjus <Torjus24sfj@gmail.com>
2025-02-14 14:49:39 +0100

    fix stories + documentation

--- src/Components/SearchComponent/SearchComponent.stories.tsx
+++ src/Components/SearchComponent/SearchComponent.stories.tsx
@@ -1,42 +1,23 @@
-/**
- * In this file you specify how to render the component inside storybook.
- * There is some boilerplate setup required to configure rendering and such.
- * Just copy these over from an existing story.
- */
 import type { Meta, StoryObj } from "@storybook/react";
-
-/** The component(s) that should be documented here */
 import { SearchComponent } from "./SearchComponent";
 
 /**
-* 
+ * The SearchComponent allows users to enter a search term and submit it.  
+ * This story demonstrates its functionality, including how to handle user input  
+ * with the `onSearch` callback. 
  */
-const meta = {
+
+const meta: Meta<typeof SearchComponent> = {
   title: "Components/SearchComponent",
   component: SearchComponent,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: "centered",
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {},
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: {},
-} satisfies Meta<typeof SearchComponent>;
+};
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof SearchComponent>;
 
-/**
- * Her starts the variations you want story book
- * to display as documentation. Each export
- * becomes one displayed item.
- *
- * More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
- */
 export const Default: Story = {
   args: {
+    onSearch: (value) => console.log("User searched for:", value),
   },
 };
