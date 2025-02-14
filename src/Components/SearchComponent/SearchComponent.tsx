e69dcc1b4a206b0f633869a081d31366c365006b
Torjus <Torjus24sfj@gmail.com>
2025-02-14 14:37:55 +0100

    making components reusable

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -2,38 +2,48 @@
 import style from "./SearchComponent.module.css";
 import { IoSearch } from "react-icons/io5";
 import { useState } from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
-export function SearchComponent() {
+interface SearchComponentProps {
+  onSearch: (value: string) => void;
+}
+
+export function SearchComponent({ onSearch }: SearchComponentProps) {
   const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearch = (search: React.FormEvent<HTMLFormElement>) => {
     search.preventDefault();
-    console.log(searchTerm);
+    onSearch(searchTerm);
     setSearchTerm("");
   };
 
-  /*Animation with framer motion*/
-  const iconAnimation = {
-    hover: { scale: 1.2, rotate: 15 },
-    tap: { scale: 0.9, rotate: -15 },
-    transition: { duration: 0.3, ease: "easeInOut" },
-  };
+    /*Animation with framer motion*/
+    const iconAnimation = {
+      hover: { scale: 1.2, rotate: 15 },
+      tap: { scale: 0.9, rotate: -15 },
+      transition: { duration: 0.3, ease: "easeInOut" },
+    };
 
-  return (
-    <form onSubmit={handleSearch} className={style.search_bar_flex_container}>
-      <motion.button className={style.search_magnifying_icon} type="submit">
-        <motion.div whileHover="hover" whileTap="tap" variants={iconAnimation}>
-          <IoSearch className={style.magnifying_icon} />
-        </motion.div>
-      </motion.button>
-      <input
-        className={style.search_input_field}
-        type="text"
-        placeholder="Search"
-        value={searchTerm}
-        onChange={(search) => setSearchTerm(search.target.value)}
-      />
-    </form>
-  );
-}
+    return (
+      <form onSubmit={handleSearch} className={style.search_bar_flex_container}>
+        <motion.button className={style.search_magnifying_icon} type="submit">
+          <motion.div
+            whileHover="hover"
+            whileTap="tap"
+            variants={iconAnimation}
+          >
+            <IoSearch className={style.magnifying_icon} />
+          </motion.div>
+        </motion.button>
+        <input
+          className={style.search_input_field}
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={(search) => setSearchTerm(search.target.value)}
+        />
+      </form>
+    );
+  }
+
+
