c3222f72bf8e8ea775237a0e14a1abf71f2b937d
Torjus <Torjus24sfj@gmail.com>
2025-02-14 09:36:59 +0100

    logic that doenst logic

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -1,11 +1,15 @@
 // @ts-expect-error cssmodules
 import style from "./SearchComponent.module.css";
 import { IoSearch } from "react-icons/io5";
+import { useState } from "react";
 
 export function SearchComponent() {
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(searchTerm)
+
   return (
     <div className={style.search_bar_flex_container}>
-      <input className={style.search_input_field} type="text" />
+      <input className={style.search_input_field} type="text" value={searchTerm} onChange={(search) => setSearchTerm(search.target.value)}/>
       <button className={style.search_magnifying_icon}>
         <IoSearch className={style.magnifying_icon}/>
       </button>
