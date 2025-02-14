aadf5615c358c173708a3e2f1e1fc4471ad56052
Torjus <Torjus24sfj@gmail.com>
2025-02-14 14:09:18 +0100

    small animation

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -1,7 +1,8 @@
-// @ts-expect-error cssmodules
+// @ts-expect-error modulecss
 import style from "./SearchComponent.module.css";
 import { IoSearch } from "react-icons/io5";
 import { useState } from "react";
+import { motion } from "framer-motion"; // Make sure to import from 'framer-motion'
 
 export function SearchComponent() {
   const [searchTerm, setSearchTerm] = useState("");
@@ -12,11 +13,20 @@ export function SearchComponent() {
     setSearchTerm("");
   };
 
+  /*Animation with motion*/
+  const iconAnimation = {
+    hover: { scale: 1.2, rotate: 15 },
+    tap: { scale: 0.9, rotate: -15 },
+    transition: { duration: 0.3, ease: "easeInOut" },
+  };
+
   return (
     <form onSubmit={handleSearch} className={style.search_bar_flex_container}>
-      <button className={style.search_magnifying_icon} type="submit">
-        <IoSearch className={style.magnifying_icon} />
-      </button>
+      <motion.button className={style.search_magnifying_icon} type="submit">
+        <motion.div whileHover="hover" whileTap="tap" variants={iconAnimation}>
+          <IoSearch className={style.magnifying_icon} />
+        </motion.div>
+      </motion.button>
       <input
         className={style.search_input_field}
         type="text"
