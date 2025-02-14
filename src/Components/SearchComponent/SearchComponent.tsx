aee3ef271c60e5b51aaab710410e7b632792803c
Torjus <Torjus24sfj@gmail.com>
2025-02-14 12:01:01 +0100

    New style

--- src/Components/SearchComponent/SearchComponent.tsx
+++ src/Components/SearchComponent/SearchComponent.tsx
@@ -6,18 +6,24 @@ import { useState } from "react";
 export function SearchComponent() {
   const [searchTerm, setSearchTerm] = useState("");
 
-  const handleSearch = (search : React.FormEvent<HTMLFormElement>) => {
-      search.preventDefault();
-      console.log(searchTerm);
-      setSearchTerm("");
+  const handleSearch = (search: React.FormEvent<HTMLFormElement>) => {
+    search.preventDefault();
+    console.log(searchTerm);
+    setSearchTerm("");
   };
 
   return (
     <form onSubmit={handleSearch} className={style.search_bar_flex_container}>
-      <input className={style.search_input_field} type="text" value={searchTerm} onChange={(search) => setSearchTerm(search.target.value)}/>
       <button className={style.search_magnifying_icon} type="submit">
-        <IoSearch className={style.magnifying_icon}/>
+        <IoSearch className={style.magnifying_icon} />
       </button>
+      <input
+        className={style.search_input_field}
+        type="text"
+        placeholder="Search"
+        value={searchTerm}
+        onChange={(search) => setSearchTerm(search.target.value)}
+      />
     </form>
   );
 }
