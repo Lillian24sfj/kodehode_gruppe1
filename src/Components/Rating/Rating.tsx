db38555e2a6cb7245cf9fc8e1d4f3ea7d3d13a54
Torjus <Torjus24sfj@gmail.com>
2025-02-17 12:17:59 +0100

    Logic and styling

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -1,18 +1,24 @@
 // @ts-expect-error modulecss
 import style from "./Rating.module.css";
 import { FaStar } from "react-icons/fa";
+import { useState } from "react";
 
 export function Rating() {
+  const [ratings, setRatings] = useState<number>(0);
+
   return (
-    <>
-      <div className={style.rating_flex_container}>
-        <FaStar id={style.rating_star} fill="none" stroke="black" strokeWidth="15"/>
-        <FaStar id={style.rating_star} fill="none" stroke="black" strokeWidth="15"/>
-        <FaStar id={style.rating_star} fill="none" stroke="black" strokeWidth="15"/>
-        <FaStar id={style.rating_star} fill="none" stroke="black" strokeWidth="15"/>
-        <FaStar id={style.rating_star} fill="none" stroke="black" strokeWidth="15"/>
-      </div>
-    </>
+    <div className={style.rating_flex_container}>
+      {[1, 2, 3, 4, 5].map((star) => (
+        <FaStar
+          key={star}
+          className={style.rating_star}
+          fill={star <= ratings ? "#fadb14" : "#f0f0f0"}
+          stroke="none"
+          strokeWidth="15"
+          onMouseEnter={() => setRatings(star)}
+          onMouseLeave={() => setRatings(0)}
+        />
+      ))}
+    </div> 
   );
 }
-
