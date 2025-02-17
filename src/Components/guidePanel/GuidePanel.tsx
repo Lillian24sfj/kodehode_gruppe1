cb7c09eafc0a41b40e1076d1bb34989a3a672b89
johannes-code <johannes24sfj@gmail.com>
2025-02-17 10:59:50 +0100

    added some example styling

--- src/Components/guidePanel/GuidePanel.tsx
+++ src/Components/guidePanel/GuidePanel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { RxAvatar } from "react-icons/rx";
-
+import styles from "./GuidePanel.module.css"
 
 interface GuidePanelProps {
   message: string;
@@ -11,9 +11,9 @@ export const GuidePanel: React.FC<GuidePanelProps> = ({ message, isVisible }) =>
   if (!isVisible) return null;
 
   return (
-    <div className="panelContainer">
-      <RxAvatar className="RxAvatar"/>
-      <p className="panelText">{message}</p>
+    <div className={styles.panelContainer}>
+      <RxAvatar className={styles.RxAvatar}/>
+      <p className={styles.panelText}>{message}</p>
     </div>
   );
 };
