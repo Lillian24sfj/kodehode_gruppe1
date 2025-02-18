8dcfad5e9698af36c3d826fadc9155b2b14958ac
Torjus <Torjus24sfj@gmail.com>
2025-02-18 11:24:53 +0100

    props

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -1,16 +1,32 @@
 // @ts-expect-error modulecss
 import style from "./Rating.module.css";
-import { FaStar } from "react-icons/fa";
 import { useState } from "react";
+import { FaStar } from "react-icons/fa";
 
-export function Rating() {
-  const [rating, setRating] = useState(0);
+interface RatingProps {
+  initialRating?: number; // Optional initial rating
+  maxStars?: number; // Max stars 
+  starSize?: number; // Star size
+  activeColor?: string; // Color for hover stars 
+  inactiveColor?: string; // Color for inactive stars
+}
+// rating - gets the user input value
+
+export function Rating({
+  initialRating = 0,
+  maxStars = 5,
+  starSize = 20,
+  activeColor = "#fadb14",
+  inactiveColor = "#f0f0f0",
+}: RatingProps) {
+  const [rating, setRating] = useState(initialRating);
   const [hover, setHover] = useState(0);
 
   return (
     <div className={style.rating_flex_container}>
-      {[...Array(5)].map((_star, i) => {
+      {[...Array(maxStars)].map((_, i) => {
         const ratingValue = i + 1;
+
 
         return (
           <label key={ratingValue}>
@@ -19,12 +35,14 @@ export function Rating() {
               name="rating"
               value={ratingValue}
               onClick={() => setRating(ratingValue)}
-              style={{ display: "none" }} 
+              style={{ display: "none" }}
             />
             <FaStar
-              className="star"
-              size={20}
-              color={ratingValue <= (hover || rating) ? "#fadb14" : "#f0f0f0"}
+              className={style.star}
+              size={starSize}
+              color={
+                ratingValue <= (hover || rating) ? activeColor : inactiveColor
+              }
               onMouseEnter={() => setHover(ratingValue)}
               onMouseLeave={() => setHover(0)}
             />
