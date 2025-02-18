d47f596d58e436557fb41b2368d503d1ee72b982
Torjus <Torjus24sfj@gmail.com>
2025-02-18 10:05:52 +0100

    placeholder value star

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -9,7 +9,7 @@ export function Rating() {
 
   return (
     <div className={style.rating_flex_container}>
-      {[...Array(5)].map((star, i) => {
+      {[...Array(5)].map((_star, i) => {
         const ratingValue = i + 1;
 
         return (
@@ -34,45 +34,3 @@ export function Rating() {
     </div>
   );
 }
-
-
-//! OVERCOMPLICATED
-
-// import { FaStar } from "react-icons/fa";
-// import { useState, useRef } from "react";
-
-// export function Rating() {
-// const [ratings, setRatings] = useState<number>(0);
-// const [selectedRating, setSelectedRating] = useState<number>(0);
-// const timeoutRef = useRef<number | null>(null);
-
-// const handleMouseEnter = (star: number) => {
-//   if (timeoutRef.current) clearTimeout(timeoutRef.current);
-//   timeoutRef.current = setTimeout(() => setRatings(star), 100);
-// };
-
-// const handleMouseLeave = () => {
-//   if (timeoutRef.current) {
-//     clearTimeout(timeoutRef.current);
-//     if (selectedRating === 0) {
-//       setRatings(0);
-//     }
-//   }
-// };
-
-// return (
-//   <div className={style.rating_flex_container}>
-//     {[1, 2, 3, 4, 5].map((star) => (
-//       <FaStar
-//         key={star}
-//         className={style.rating_star}
-//         data-star={star}
-//         fill={star <= (ratings || selectedRating) ? "#fadb14" : "#f0f0f0"}
-//         onMouseEnter={() => handleMouseEnter(star)}
-//         onMouseLeave={handleMouseLeave}
-//         onClick={() => setSelectedRating(star === selectedRating ? 0 : star)}
-//       />
-//     ))}
-//   </div>
-// );
-// }
