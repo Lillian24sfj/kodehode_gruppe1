15d07e0773801fe1298e655e882868531a296527
Lillian24sfj <lillian24sfj@gmail.com>
2025-02-18 13:22:49 +0100

    Satt opp pre commit hooks
    * Keep calm and commit
    
    * Keep calm and commit
    
    * Keep calm and commit
    
    * Keep calm and commit
    
    * Keep calm and commit
    
    * Ferdig..

--- src/Components/guidePanel/GuidePanel.tsx
+++ src/Components/guidePanel/GuidePanel.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { RxAvatar } from "react-icons/rx";
-import styles from "./GuidePanel.module.css"
+import styles from "./GuidePanel.module.css";
 
 interface GuidePanelProps {
   message: string;
   isVisible: boolean;
 }
 
-export const GuidePanel: React.FC<GuidePanelProps> = ({ message, isVisible }) => {
+export const GuidePanel: React.FC<GuidePanelProps> = ({
+  message,
+  isVisible,
+}) => {
   if (!isVisible) return null;
 
   return (
     <div className={styles.panelContainer}>
-      <RxAvatar className={styles.RxAvatar}/>
+      <RxAvatar className={styles.RxAvatar} />
       <p className={styles.panelText}>{message}</p>
     </div>
   );
