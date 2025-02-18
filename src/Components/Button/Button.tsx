4e366775cab8741d9481fa4d3ed734a50b60cb02
Torjus <Torjus24sfj@gmail.com>
2025-02-18 13:02:08 +0100

    stories error

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,11 +1,10 @@
-// @ts-expect-error modulecss
 import style from "./Button.module.css"
 
 export function Button() {
 
     return (
         <>
-        
+        <div><h1 className={style.test}>TesTTTTTTT</h1></div>
         </>
     )
 }
