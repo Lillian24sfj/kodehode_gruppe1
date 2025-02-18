2fe9d9258d317ae22f3a974f0b73dbb905f114d4
Torjus <Torjus24sfj@gmail.com>
2025-02-18 09:53:32 +0100

    signature

--- src/Components/SignatureTHT/SignatureTHT.tsx
+++ src/Components/SignatureTHT/SignatureTHT.tsx
@@ -1,10 +1,6 @@
 export function SignatureTHT() {
     return (
-    //   <a href="https://github.com/Torjus24SFJ">
-        <p>
-            something clever
-        </p>
-    //   </a>
+        <p>Torjus Signature</p>
     )
   }
   
