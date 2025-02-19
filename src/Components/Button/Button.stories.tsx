1abcf5dfb4ca44c82410f675594317061c8f735e
Torjus <Torjus24sfj@gmail.com>
2025-02-19 12:13:37 +0100

    removed to many customize props

--- src/Components/Button/Button.stories.tsx
+++ src/Components/Button/Button.stories.tsx
@@ -2,13 +2,14 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./Button";
 
 /**
- * WRITE DOCUMENTATION HERE!
+ * A customizable button component with color, text color, border, and size options
  */
 
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
   tags: ["autodocs"],
+
 };
 
 export default meta;
@@ -19,3 +20,5 @@ export const Default: Story = {
    
   },
 };
+
+
--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,23 +3,36 @@ import style from "./Button.module.css";
 interface ButtonProps {
   text: string;
   onClick: () => void;
-  color?: 'primary' | 'secondary' | 'tertiary';
-  size?: 'small' | 'medium' | 'large';
-  disabled: boolean;
+  color?: string;
+  textColor?: string;
+  hoverEffect?: boolean,
+  size?: "small" | "medium" | "large";
+  // border?: number;
+  // borderColor?: string;
 }
 
 export function Button({
-  text,
-  onClick,
+  text = "Click me!",
   size = "medium",
-  color = 'primary',
-  disabled = false,
-
-  
+  color = "#1e293b",
+  textColor = "#f1f1f1",
+  hoverEffect = true,
+  onClick,
+  // border = 0,
+  // borderColor = "transparent",
 }: ButtonProps) {
-  const buttonClass = `${style.button} ${style[size]} ${style[color]} ${style[disabled ? style.disabled : '']}`
+  const buttonClass = `${style.button_style} ${style[size]} ${hoverEffect ? style.hover : ""}`;
   return (
-    <button  className={buttonClass} onClick={onClick} disabled={disabled}>
+    <button
+      className={buttonClass}
+      onClick={onClick}
+      style={{
+        backgroundColor: color,
+        color: textColor,
+        // border: `${border}px solid ${borderColor}`,
+        
+      }}
+    >
       {text}
     </button>
   );
