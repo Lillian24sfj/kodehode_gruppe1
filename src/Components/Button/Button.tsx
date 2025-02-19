843ab0950465434d4b73eaf592f269628558bd0b
Torjus <Torjus24sfj@gmail.com>
2025-02-19 09:41:16 +0100

    buttonclass properties

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -3,18 +3,23 @@ import style from "./Button.module.css";
 interface ButtonProps {
   text: string;
   onClick: () => void;
-  color: "primary" | "secondary" | "tertiary";
-  padding?: number;
-  size: number;
+  color?: 'primary' | 'secondary' | 'tertiary';
+  size?: 'small' | 'medium' | 'large';
   disabled: boolean;
 }
 
 export function Button({
   text,
   onClick,
+  size = "medium",
+  color = 'primary',
+  disabled = false,
+
+  
 }: ButtonProps) {
+  const buttonClass = `${style.button} ${style[size]} ${style[color]} ${style[disabled ? style.disabled : '']}`
   return (
-    <button className={style.button_style} onClick={onClick}>
+    <button  className={buttonClass} onClick={onClick} disabled={disabled}>
       {text}
     </button>
   );
