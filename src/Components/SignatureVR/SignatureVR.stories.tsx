c9f50ce3848a247c369126a8ca41d90ab18404bf
vegar24sfj <vegar24sfj@gmail.com>
2025-02-25 10:52:45 +0100

    endret hele SignatureVR

--- src/Components/SignatureVR/SignatureVR.stories.tsx
+++ src/Components/SignatureVR/SignatureVR.stories.tsx
@@ -1,37 +1,64 @@
-import { Meta, StoryObj } from "@storybook/react";
-import { SignatureVR } from "./SignatureVR"; // Make sure the path is correct
-import { SignatureVRProps } from "./SignatureVR"; // Import the type
+/**
+ * In this file you specify how to render the component inside storybook.
+ * There is some boilerplate setup required to configure rendering and such.
+ * Just copy these over from an existing story.
+ */
+import type { Meta, StoryObj } from "@storybook/react";
 
-export default {
+/** The component(s) that should be documented here */
+import { SignatureVR } from "./SignatureVR";
+
+/**
+ * Meta information about this page and how to render this component
+ * can be configured here. Of note is the title it should be displayed under,
+ * along with any organizational structures like root directory.
+ *
+ * More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+ */
+const meta = {
   title: "Components/SignatureVR",
   component: SignatureVR,
-  argTypes: {
-    onOpenChange: { action: "opened/closed" }, // This will show actions in Storybook
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: "centered",
   },
-} as Meta;
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ["autodocs"],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: {},
+} satisfies Meta<typeof SignatureVR>;
 
-// Template with SignatureVRProps type
-const Template: StoryObj<SignatureVRProps> = {
-  render: (args) => <SignatureVR {...args} />,
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+/**
+ * Her starts the variations you want story book
+ * to display as documentation. Each export
+ * becomes one displayed item.
+ *
+ * More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+ */
+export const Default: Story = {
+  args: {
+    children: <p>Example child</p>,
+  },
 };
 
-// Default story example
-export const Default: StoryObj<SignatureVRProps> = {
-  ...Template,
+/**
+ * The JSDoc comment tags (like this one), can
+ * be used alongside the "autodoc" feature of
+ * Storybook to automatically use these as documentation
+ * for the rendered component.
+ */
+export const LargeChildComponent: Story = {
   args: {
-    header: "Dette regnes som helsemessige begrensninger",
-    children:
-      "Med helsemessige begrensninger mener vi funksjonshemming, sykdom, allergier som hindrer deg i arbeidet eller andre årsaker...",
-  },
-};
-
-// OpenByDefault story example
-export const OpenByDefault: StoryObj<SignatureVRProps> = {
-  ...Template,
-  args: {
-    header: "Dette regnes som helsemessige begrensninger",
-    children:
-      "Med helsemessige begrensninger mener vi funksjonshemming, sykdom, allergier som hindrer deg i arbeidet eller andre årsaker...",
-    defaultOpen: true,
+    children: (
+      <div>
+        <p>A more complex child component!</p>
+        <p>Now with multiple lines!</p>
+      </div>
+    ),
   },
 };
--- src/Components/SignatureVR/SignatureVR.tsx
+++ src/Components/SignatureVR/SignatureVR.tsx
@@ -1,46 +1,9 @@
-import React, { useState } from "react";
-import './SignatureVR'; // Import the custom CSS
-
-export interface SignatureVRProps {
-  header: React.ReactNode;
-  children: React.ReactNode;
-  defaultOpen?: boolean;
-  open?: boolean;
-  onOpenChange?: (open: boolean) => void;
-  size?: "small" | "medium" | "large";
-}
-
-export const SignatureVR: React.FC<SignatureVRProps> = ({
-  header,
-  children,
-  defaultOpen = false,
-  open: controlledOpen,
-  onOpenChange,
-  size = "medium",
-}) => {
-  const [isOpen, setIsOpen] = useState(defaultOpen);
-  const open = controlledOpen !== undefined ? controlledOpen : isOpen;
-
-  const toggleOpen = () => {
-    if (onOpenChange) {
-      onOpenChange(!open);
-    }
-    setIsOpen(!open);
-  };
-
+export function SignatureVR() {
   return (
-    <div className={`signature-vr signature-vr-${size}`}>
-      <div className="signature-vr-header">
-        <button onClick={toggleOpen}>
-          {open ? "Show less" : "Read more"}
-        </button>
-        <h3>{header}</h3>
-      </div>
-      {open && (
-        <div className="signature-vr-content">
-          {children}
-        </div>
-      )}
-    </div>
-  );
-};
+  //   <a href="https://github.com/vegar24sfj">
+      <p>
+          something clever
+      </p>
+  //   </a>
+  )
+}
